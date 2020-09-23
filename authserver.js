897e9f5a3e9c1b9249fdadd693406fe5fb331640
jaspreet145 <jaspreetsingh145@gmail.com>
2020-09-23 20:51:55 +0530

    fixed on reload server error

--- authserver.js
+++ authserver.js
@@ -2,12 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(morgan("tiny"));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
