5c66d24f054c9f35ae7c0f0db1de51b0bb52a0ce
jaspreet145 <jaspreetsingh145@gmail.com>
2020-09-25 16:10:57 +0530

    Add Responsive table

--- client/src/components/ResultElement.component.js
+++ client/src/components/ResultElement.component.js
@@ -4,9 +4,24 @@ import styles from "../componentsStyles/TestResult.module.css";
 function Resultelement(props) {
   return (
     <div className={styles.resultrow}>
-      <div className={styles.element}>{props.name}</div>
-      <div className={styles.element}>{props.email}</div>
-      <div className={styles.element}>{props.score}%</div>
+      <div className={styles.element}>
+        <span className={styles.mobileinfo}>
+          <strong>Name : </strong>
+        </span>
+        {props.name}
+      </div>
+      <div className={styles.element}>
+        <span className={styles.mobileinfo}>
+          <strong>Email : </strong>
+        </span>
+        {props.email}
+      </div>
+      <div className={styles.element}>
+        <span className={styles.mobileinfo}>
+          <strong>Score : </strong>
+        </span>
+        {props.score}%
+      </div>
     </div>
   );
 }
