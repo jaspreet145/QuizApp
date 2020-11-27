1e190cbd9e298fd7d19c317e021cecb348a56b3e
Jaspreet Singh <36356867+jaspreet145@users.noreply.github.com>
2020-11-27 11:05:43 +0530

    timer bug fix initial formatting

--- client/src/components/Timer.component.js
+++ client/src/components/Timer.component.js
@@ -21,6 +21,15 @@ function Timer(props) {
       setsecs(altsecs);
     }
   };
+  
+  useEffect(() => {
+      let altmins = Math.floor(allsecs / 60).toString();
+      if (altmins.length == 1) altmins = "0" + altmins;
+      let altsecs = (allsecs % 60).toString();
+      if (altsecs.length == 1) altsecs = "0" + altsecs;
+      setmins(altmins);
+      setsecs(altsecs);
+  });
 
   useEffect(() => {
     sethelper(setInterval(handle, 1000));
